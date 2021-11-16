0eb53f3a8ba5b077fbbe04b2e2511d6e18333a0e
Mihrican Kızılyer <mihricankizilyer@gmail.com>
2021-11-16 22:39:11 +0300

    2.day

---
+++ 02_very_big_numbers.py
@@ -0,0 +1,26 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+def aVeryBigSum(ar):
+    k = 0
+    for i in ar:
+        k = k + i
+    return k
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    ar_count = int(input().strip())
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = aVeryBigSum(ar)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
