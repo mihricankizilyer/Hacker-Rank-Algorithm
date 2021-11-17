51fcae86a0f3f00dfac4137cea74c276addfa72a
Mihrican Kızılyer <mihricankizilyer@gmail.com>
2021-11-17 23:23:20 +0300

    03.day

--- 03_compare_the_triplets.py
+++ 03_compare_the_triplets.py
@@ -6,15 +6,6 @@ import random
 import re
 import sys
 
-#
-# Complete the 'compareTriplets' function below.
-#
-# The function is expected to return an INTEGER_ARRAY.
-# The function accepts following parameters:
-#  1. INTEGER_ARRAY a
-#  2. INTEGER_ARRAY b
-#
-
 def compareTriplets(a, b):
     a_val = 0
     b_val = 0
