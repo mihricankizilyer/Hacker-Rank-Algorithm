405ac9c7c923110ee9acbcd45095bae2adb91b0e
Mihrican Kızılyer <70093355+mihricankizilyer@users.noreply.github.com>
2021-12-12 10:36:26 +0300

    01_day

--- 01_array_sum.py
+++ 01_array_sum.py
@@ -1,7 +1,28 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
 def control(arr):
     y = []
     for i in range(arr):
         y.append(int(input()))
-    toplam = sum(y)
-    return toplam
+    sum1 = sum(y)
+    return sum1
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    ar_count = int(input().strip())
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = simpleArraySum(ar)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
 
